19e91334ebd2a3c00289b050cd066a66e3e7bca1
Nojus Gataveckas <nojus.gataveckas@ktu.edu>
2021-04-29 23:56:04 +0300

    Img fix

--- resources/js/app/components/About.js
+++ resources/js/app/components/About.js
@@ -127,7 +127,7 @@ class About extends Component{
                                         <h3>Credits</h3>
                                         <h5>TheMovieDB</h5>
                                         <p>As per TheMovieDB terms of use, every application that uses TheMovieDB data or images is required to properly attribute TMDb as the source. More information about TheMovieDB <a href="https://www.themoviedb.org/">here</a></p>
-                                        <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt = "TheMovieDB logo"></img>
+                                        <img class="tmdbLogo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt = "TheMovieDB logo"></img>
                                     </Col>
                                 </Row>
                                 </div> : null }

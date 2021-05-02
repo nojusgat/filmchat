c65e913c141c623a3db5ec7d76c44bb30477df53
DDumanTT <haiwerss@gmail.com>
2021-05-02 03:01:27 +0300

    Added pagination and search

--- resources/js/app/components/Users.js
+++ resources/js/app/components/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import AppNavbar from "../child-components/AppNavbar";
 import {
     Button,
@@ -6,55 +7,146 @@ import {
     Row,
     Col,
     Spinner,
-    Pagination,
-    PaginationItem,
-    PaginationLink,
     InputGroup,
+    InputGroupAddon,
+    Input,
 } from "reactstrap";
 import FriendsService from "../services/FriendsService";
 import UserCard from "../child-components/UserCard";
+import Paginate from "../child-components/Paginate";
+import { AiOutlineSearch } from "react-icons/ai";
 
 class Users extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
+        this.state = this.props.location.state || {
             usersList: [],
-            page: 0,
-            total_pages: 0,
-            isOpenDrop: false,
-            search: "",
-            additional: null,
-            method: "popular",
-            blockName: "Users",
+            currentPage: 1,
+            totalPages: 0,
+            perPage: 20,
+            blockName: "Users:",
             isLoading: true,
+            search: "",
+            method: "all",
         };
+
+        this.showUsers.bind(this);
     }
 
     componentDidMount() {
         if (this.state.usersList.length == 0) {
-            this.showUsers();
+            this.showUsers(this.state.currentPage);
         }
-        // console.log(this.state.currentUser);
     }
 
-    showUsers() {
-        FriendsService.getUsers().then(
-            (response) => {
-                this.setState({ usersList: response.data, isLoading: false });
-                // console.log(this.state.usersList);
-            },
-            (error) => {
-                console.log("Error in getUsers: " + error.toString());
-            }
-        );
-    }
+    showUsers = (page) => {
+        this.setState({ isLoading: true });
+        console.log(this.state.method);
+        switch (this.state.method) {
+            case "all":
+                FriendsService.getUsers(page, this.state.perPage).then(
+                    (response) => {
+                        this.setState({
+                            usersList: response.data.users,
+                            isLoading: false,
+                            currentPage: page,
+                            totalPages: response.data.count,
+                        });
+                        this.props.history.push({ state: this.state });
+                    },
+                    (error) => {
+                        console.log("Error in getUsers: " + error.toString());
+                    }
+                );
+                break;
+            case "search":
+                FriendsService.searchUsers(
+                    page,
+                    this.state.perPage,
+                    this.state.search
+                ).then(
+                    (response) => {
+                        this.setState({
+                            usersList: response.data.users,
+                            isLoading: false,
+                            currentPage: page,
+                            totalPages: response.data.count,
+                        });
+                        this.props.history.push({ state: this.state });
+                    },
+                    (error) => {
+                        console.log(
+                            "Error in searchUsers: " + error.toString()
+                        );
+                    }
+                );
+                break;
+            default:
+                break;
+        }
+    };
+
+    changeHandler = (e) => {
+        this.setState({ search: e.target.value });
+    };
+
+    searchHandler = () => {
+        if (this.state.search === "") {
+            this.setState(
+                {
+                    method: "all",
+                    blockName: "Users:",
+                    isLoading: true,
+                },
+                () => {
+                    this.showUsers(1);
+                }
+            );
+        } else {
+            this.setState(
+                {
+                    method: "search",
+                    blockName: 'Searching "' + this.state.search + '":',
+                    isLoading: true,
+                },
+                () => {
+                    this.showUsers(1);
+                }
+            );
+        }
+    };
 
     render() {
         return (
             <div>
                 <AppNavbar />
                 <Container fluid>
+                    <Row>
+                        <Col
+                            style={{ marginTop: "20px" }}
+                            md={{ size: 4, offset: 2 }}
+                        >
+                            <InputGroup>
+                                <Input
+                                    placeholder="Search users..."
+                                    defaultValue={this.state.search}
+                                    name="search"
+                                    onChange={this.changeHandler}
+                                />
+                                <InputGroupAddon addonType="append">
+                                    <Button
+                                        outline
+                                        color="info"
+                                        name="search"
+                                        onClick={this.searchHandler}
+                                    >
+                                        <AiOutlineSearch name="search" />
+                                    </Button>
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </Col>
+                    </Row>
                     <Row style={{ marginTop: "10px" }}>
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
                             <h1 style={{ marginTop: "20px" }}>
@@ -77,8 +169,55 @@ class Users extends Component {
                                         </div>
                                     );
                                 } else {
+                                    const listUsers = this.state.usersList.map(
+                                        function (data) {
+                                            return (
+                                                <Col
+                                                    md="6"
+                                                    xl="3"
+                                                    sm="12"
+                                                    className="mb-3"
+                                                    key={data.id.toString()}
+                                                >
+                                                    <UserCard data={data} />
+                                                </Col>
+                                            );
+                                        }
+                                    );
+
                                     return (
-                                        <UserCard data={this.state.usersList} />
+                                        <div style={{ marginTop: "20px" }}>
+                                            <Row>
+                                                <Paginate
+                                                    setPage={this.showUsers}
+                                                    totalPages={
+                                                        this.state.totalPages
+                                                    }
+                                                    currentPage={
+                                                        this.state.currentPage
+                                                    }
+                                                    perPage={this.state.perPage}
+                                                />
+                                            </Row>
+                                            <Row className="mb-2">
+                                                {this.state.usersList.length ==
+                                                0
+                                                    ? "Users not found."
+                                                    : listUsers}
+                                            </Row>
+                                            <Row>
+                                                <Paginate
+                                                    setPage={this.showUsers}
+                                                    totalPages={
+                                                        this.state.totalPages
+                                                    }
+                                                    currentPage={
+                                                        this.state.currentPage
+                                                    }
+                                                    perPage={this.state.perPage}
+                                                />
+                                            </Row>
+                                        </div>
                                     );
                                 }
                             })()}
@@ -90,4 +229,4 @@ class Users extends Component {
     }
 }
 
-export default Users;
+export default withRouter(Users);

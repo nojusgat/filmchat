af76829d9fb8f8ee753347a95f7c2b9bbb009c51
DDumanTT <haiwerss@gmail.com>
2021-05-02 03:01:42 +0300

    Added notification sending

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,12 @@ import MainApp from './MainApp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.js';
 
+import ReactNotification from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
+
 ReactDOM.render(
   <React.Fragment>
+    <ReactNotification/>
     <MainApp />
   </React.Fragment>,
   document.getElementById('root')
